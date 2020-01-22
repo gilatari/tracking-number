10c6f71f352c588157c95de36ba81590f26e3af8
Gil Atari <gil.atari@gmail.com>
2020-01-23 01:48:40 +0200

    Updates

---
+++ precache-manifest.5b36e311dcbe3f411a98e05e6c3eb283.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef231af5b4f43dec1f58b9ad101fd5d5",
+    "url": "/tracking-number/index.html"
+  },
+  {
+    "revision": "f56153f78e0d3106cab7",
+    "url": "/tracking-number/static/css/main.884931e4.chunk.css"
+  },
+  {
+    "revision": "879e2822bcbca1cddaf8",
+    "url": "/tracking-number/static/js/2.23ddbd4f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tracking-number/static/js/2.23ddbd4f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f56153f78e0d3106cab7",
+    "url": "/tracking-number/static/js/main.9b743d89.chunk.js"
+  },
+  {
+    "revision": "9ddc2634848767fd2a62",
+    "url": "/tracking-number/static/js/runtime-main.5a5e5195.js"
+  }
+]);
